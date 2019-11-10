0991df48ca988516e734500c6835f0d03a16879f
Andre Souza <andresouza4@gmail.com>
2019-11-09 20:41:39 -0800

    Add Eslint + Prettier

--- src/views/facilities/map-view.js
+++ src/views/facilities/map-view.js
@@ -1,25 +1,31 @@
-import React, { Component } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api'
 
 // TODO: move to config file
-const GOOGLE_MAPS_API_KEY = "AIzaSyDSLM-Bv5YwI1Ecw2OrMDQF8fZxik6FTzs"
+const GOOGLE_MAPS_API_KEY = 'AIzaSyDSLM-Bv5YwI1Ecw2OrMDQF8fZxik6FTzs'
 
-const MapView = props =>
-	<LoadScript
-		id="script-loader"
-		googleMapsApiKey={GOOGLE_MAPS_API_KEY}
-	>
-		<GoogleMap
-			id="circle-example"
-			mapContainerStyle={{
-				height: "400px",
-				width: "100%"
-			}}
-			zoom={16}
-			center={props.center}
-		>
-			<Marker position={props.center} /> 
-		</GoogleMap>
-	</LoadScript>
+const MapView = ({ center }) => (
+  <LoadScript id="script-loader" googleMapsApiKey={GOOGLE_MAPS_API_KEY}>
+    <GoogleMap
+      id="circle-example"
+      mapContainerStyle={{
+        height: '400px',
+        width: '100%'
+      }}
+      zoom={16}
+      center={center}
+    >
+      <Marker position={center} />
+    </GoogleMap>
+  </LoadScript>
+)
+
+MapView.propTypes = {
+  center: PropTypes.shape({
+    lat: PropTypes.number,
+    long: PropTypes.number
+  }).isRequired
+}
 
 export default MapView
