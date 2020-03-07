c0eed6b097954cad4fe766eb0f7263c45e07fccc
Juliani Schlickmann Damasceno <juliani.schlickmann@gmail.com>
2020-03-07 11:50:30 -0800

    Removed # from url and fixed eslint errors

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // in src/App.js
 import React from 'react'
 import { Admin, Resource } from 'react-admin'
+import { createBrowserHistory as createHistory } from 'history'
 import jsonServerProvider from 'ra-data-json-server'
 import PostIcon from '@material-ui/icons/Book'
 import UserIcon from '@material-ui/icons/Group'
@@ -9,11 +10,14 @@ import Dashboard from './Dashboard'
 import { FacilityList, FacilityEdit } from './views/facilities'
 import { UserList, UserEdit, UserCreate } from './views/users'
 
+const history = createHistory()
+
 // const dataProvider = jsonServerProvider('https://my-json-server.typicode.com/linkvan/linkvan-admin');
 const dataProvider = jsonServerProvider('http://localhost:3000')
 
 const App = () => (
   <Admin
+    history={history}
     dashboard={Dashboard}
     authProvider={authProvider}
     dataProvider={dataProvider}
